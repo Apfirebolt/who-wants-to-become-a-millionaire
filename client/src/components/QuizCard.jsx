e0a646103231a9a422eea320bbf6fe4401acc9e7
Apfirebolt <aspper20@gmail.com>
2023-12-12 15:10:34 +0530

    changes in the question UI

--- client/src/components/QuizCard.jsx
+++ client/src/components/QuizCard.jsx
@@ -8,25 +8,29 @@ const QuizCard = (props) => {
         <div className="bg-gray-100 px-2 text-center py-3">
             <h2 className="text-lg text-red-600">{quiz.name}</h2>
             <div className="my-3">
+                
                 {quiz.questions && quiz.questions.map((question) => (
-                    <div key={question.id} className="bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                        {question.text}
-
-                        <p>
-                            {question.option1}
+                    <div key={question.id} className="bg-gray-100 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                        <p className="text-3xl my-3 font-bold">
+                            {question.text}
                         </p>
+                       <div className="grid sm:grid-cols-2 gap-4">
+                            <p className="block bg-gray-200 border border-transparent rounded-md py-3 px-8 text-base font-medium text-gray-900 hover:bg-gray-400 sm:w-auto">
+                                {question.option1}
+                            </p>
 
-                        <p>
-                            {question.option2}
-                        </p>
+                            <p className="block bg-gray-200 border border-transparent rounded-md py-3 px-8 text-base font-medium text-gray-900 hover:bg-gray-400 sm:w-auto">
+                                {question.option2}
+                            </p>
 
-                        <p>
-                            {question.option3}
-                        </p>
+                            <p className="block bg-gray-200 border border-transparent rounded-md py-3 px-8 text-base font-medium text-gray-900 hover:bg-gray-400 sm:w-auto">
+                                {question.option3}
+                            </p>
 
-                        <p>
-                            {question.option4}
-                        </p>
+                            <p className="block bg-gray-200 border border-transparent rounded-md py-3 px-8 text-base font-medium text-gray-900 hover:bg-gray-400 sm:w-auto">
+                                {question.option4}
+                            </p>
+                       </div>
                     </div>
                 ))}
             </div>
--- client/src/views/Quiz.jsx
+++ client/src/views/Quiz.jsx
@@ -19,46 +19,45 @@ const Quiz = () => {
 
   return (
     <div className="bg-gray-50">
-      <div className="mx-auto max-w-screen-xl px-4 py-16 lg:flex lg:items-center">
-        <div className="mx-auto max-w-xl text-center">
-          <h1 className="text-3xl font-extrabold sm:text-5xl">Quizes</h1>
-        </div>
-      </div>
-
-      <div className="grid grid-cols-1 px-2 gap-2 my-3">
-        {quizes && quizes.map((quiz) => (
-          <QuizCard key={quiz.id} quiz={quiz} />
-        ))}
-      </div>
-      
-      {/* Featured section */}
       <section aria-labelledby="cause-heading">
-          <div className="relative bg-gray-800 py-32 px-6 sm:py-40 sm:px-12 lg:px-16">
-            <div className="absolute inset-0 overflow-hidden">
-              <img
-                src="https://tailwindui.com/img/ecommerce-images/home-page-03-feature-section-full-width.jpg"
-                alt=""
-                className="w-full h-full object-center object-cover"
-              />
-            </div>
-            <div aria-hidden="true" className="absolute inset-0 bg-gray-900 bg-opacity-50" />
-            <div className="relative max-w-3xl mx-auto flex flex-col items-center text-center">
-              <h2 id="cause-heading" className="text-3xl font-extrabold tracking-tight text-white sm:text-4xl">
-                Long-term thinking
-              </h2>
-              
-              
+        <div className="relative bg-gray-800 py-32 px-6 sm:py-40 sm:px-12 lg:px-16">
+          <div className="absolute inset-0 overflow-hidden">
+            <img
+              src="https://tailwindui.com/img/ecommerce-images/home-page-03-feature-section-full-width.jpg"
+              alt=""
+              className="w-full h-full object-center object-cover"
+            />
+          </div>
+          <div
+            aria-hidden="true"
+            className="absolute inset-0 bg-gray-900 bg-opacity-50"
+          />
+          <div className="relative max-w-4xl mx-auto text-center">
+            <h2
+              id="cause-heading"
+              className="text-3xl font-extrabold tracking-tight text-white sm:text-4xl"
+            >
+              Quiz
+            </h2>
 
-              <a
-                href="#"
-                className="mt-8 w-full block bg-white border border-transparent rounded-md py-3 px-8 text-base font-medium text-gray-900 hover:bg-gray-100 sm:w-auto"
-              >
-                Read our story
-              </a>
+            <div className="grid grid-cols-1 px-2 gap-2 my-3">
+              {quizes &&
+                quizes.map((quiz) => <QuizCard key={quiz.id} quiz={quiz} />)}
             </div>
           </div>
-        </section>
-      
+        </div>
+
+        <div className="mx-auto mt-2 w-1/5 px-3 py-2 text-center">
+          <a
+            href="#"
+            className="block bg-gray-200 border border-transparent rounded-md py-3 px-8 text-base font-medium text-gray-900 hover:bg-gray-100 sm:w-auto"
+          >
+            Take Quiz
+          </a>
+        </div>
+      </section>
+
+      {/* Featured section */}
     </div>
   );
 };
