0b675fc4620adb2b9ae8ccddf21f7d6f1733c7c6
Apfirebolt <aspper20@gmail.com>
2023-12-25 00:23:50 +0530

    feat: add admin quiz result page

---
+++ client/src/components/QuizResultAdmin.jsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import moment from "moment";
+
+const QuizResultAdmin = (props) => {
+  const { quizTaker, deleteQuizTaker } = props;
+  const [responses, setResponses] = useState({});
+
+  const correctAnswerClasses = 'text-green-600 font-semibold text-lg';
+  const wrongAnswerClasses = 'text-red-600 font-semibold text-lg';
+
+  useEffect(() => {
+    if (quizTaker && quizTaker.responses) {
+      setResponses(JSON.parse(quizTaker.responses));
+    }
+  }, [quizTaker]);
+
+  return (
+    <div className="bg-gray-100 px-2 text-center py-3">
+      <div className="my-3">
+        <div className="bg-gray-200 py-3 px-2">
+          <p className="text-blue-600 font-semibold text-lg">
+            {quizTaker.quiz.name}
+          </p>
+
+          <p className="text-blue-600">
+            Quiz Attempted at {moment(quizTaker.createdAt).format("LLL")}
+          </p>
+
+          <p className="text-blue-600 font-semibold text-lg">
+            Score : {quizTaker.score}
+          </p>
+        </div>
+
+        <div className="grid grid-cols-3 bg-purple-900 mt-4 px-4 py-3 text-gray-100 gap-4">
+          <div>
+            <p>Questions</p>
+          </div>
+          <div>
+            <p>Your Answer</p>
+          </div>
+          <div>
+            <p>Correct Answer</p>
+          </div>
+        </div>
+        {Object.keys(responses).map((item, index) => (
+          <div
+            key={index}
+            className="grid grid-cols-3 bg-gray-900 px-4 py-3 text-gray-100 gap-4"
+          >
+            <div>
+              <p>Question : {item}</p>
+            </div>
+            <div>
+              <p className={responses[item][0] === responses[item][1] ? correctAnswerClasses : wrongAnswerClasses}>Your Answer : {responses[item][0]}</p>
+            </div>
+            <div>
+              <p>Correct Answer : {responses[item][1]}</p>
+            </div>
+          </div>
+        ))}
+        <button
+          onClick={() => deleteQuizTaker(quizTaker)}
+          className="block mx-auto my-2 bg-gray-200 border border-transparent rounded-md py-3 px-8 text-base font-medium text-gray-900 hover:bg-gray-100 sm:w-auto"
+        >
+          Delete Quiz Taker
+        </button>
+      </div>
+    </div>
+  );
+};
+
+QuizResultAdmin.propTypes = {
+  quizTaker: PropTypes.object.isRequired,
+  deleteQuizTaker: PropTypes.func.isRequired,
+};
+
+export default QuizResultAdmin;
--- client/src/views/admin/QuizTaker.jsx
+++ client/src/views/admin/QuizTaker.jsx
@@ -1,10 +1,89 @@
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  getQuizTakers,
+  deleteQuizTaker,
+} from "../../features/quizTaker/quizTakerSlice";
+import Loader from "../../components/Loader";
+import QuizResultAdmin from "../../components/QuizResultAdmin";
+import AdminMenu from "../../components/AdminMenu";
+import ConfirmModal from "../../components/ConfirmModal";
+
 const AdminQuizTaker = () => {
-    return (
-      <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-        <h1 className="text-4xl font-bold mb-8">Admin Quiz Taker Page</h1>
-      </div>
-    );
+  const { quizTakers, isLoading } = useSelector((state) => state.quizTaker);
+
+  const dispatch = useDispatch();
+  const [confirmMessage, setConfirmMessage] = useState("");
+  const [isConfirmModalOpened, setIsConfirmModalOpened] = useState(false);
+  const [selectedQuestion, setSelectedQuestion] = useState(null);
+
+  const deleteQuestionUtil = async () => {
+    await dispatch(deleteQuizTaker(selectedQuestion.id));
+    setIsConfirmModalOpened(false);
+    dispatch(getQuizTakers());
   };
-  
-  export default AdminQuizTaker;
-  
+
+  const deleteQuizTakerHandler = async (quiz) => {
+    setSelectedQuestion(quiz);
+    setConfirmMessage("Are you sure you want to delete this quiz taker?");
+    setIsConfirmModalOpened(true);
+  };
+
+  useEffect(() => {
+    dispatch(getQuizTakers());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  return (
+    <div className="flex justify-center bg-gray-100 px-4 py-3">
+      <AdminMenu />
+      <div className="w-4/5 bg-slate-300 px-3 py-5">
+        <section aria-labelledby="cause-heading">
+          <div className="relative bg-gray-800 py-4 px-6">
+            <div className="absolute inset-0 overflow-hidden">
+              <img
+                src="https://tailwindui.com/img/ecommerce-images/home-page-03-feature-section-full-width.jpg"
+                alt=""
+                className="w-full h-full object-center object-cover"
+              />
+            </div>
+            <div
+              aria-hidden="true"
+              className="absolute inset-0 bg-gray-900 bg-opacity-50"
+            />
+            <div className="relative max-w-6xl mx-auto text-center">
+              <h2
+                id="cause-heading"
+                className="text-3xl font-extrabold tracking-tight text-white sm:text-4xl"
+              >
+                Quiz Takers
+              </h2>
+
+              <div className="grid grid-cols-1 px-2 gap-1 my-3">
+                {quizTakers &&
+                  quizTakers.map((quiz) => (
+                    <QuizResultAdmin
+                      key={quiz.id}
+                      quizTaker={quiz}
+                      deleteQuizTaker={deleteQuizTakerHandler}
+                    />
+                  ))}
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+      <ConfirmModal
+        isOpen={isConfirmModalOpened}
+        closeModal={() => setIsConfirmModalOpened(false)}
+        message={confirmMessage}
+        confirmAction={deleteQuestionUtil}
+      />
+    </div>
+  );
+};
+
+export default AdminQuizTaker;
