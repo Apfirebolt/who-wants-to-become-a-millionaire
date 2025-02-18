b16bd48d208b6921c7148e331a29a705ce995cd9
Apfirebolt <aspper20@gmail.com>
2025-02-18 11:19:10 +0530

    feat: added mobile menu without headless UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,6 @@ import { useSelector, useDispatch } from "react-redux";
 import { logout } from "../features/auth/authSlice";
 import { Fragment, useState } from "react";
 import { Link } from "react-router-dom"; // Add router link
-import { Dialog, Transition } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
 const Header = () => {
@@ -18,116 +17,86 @@ const Header = () => {
   return (
     <div className="bg-white">
       {/* Mobile menu */}
-      <Transition.Root show={open} as={Fragment}>
-        <Dialog
-          as="div"
-          className="fixed inset-0 flex z-40 lg:hidden"
-          onClose={setOpen}
-        >
-          <Transition.Child
-            as={Fragment}
-            enter="transition-opacity ease-linear duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="transition-opacity ease-linear duration-300"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-25" />
-          </Transition.Child>
 
-          <Transition.Child
-            as={Fragment}
-            enter="transition ease-in-out duration-300 transform"
-            enterFrom="-translate-x-full"
-            enterTo="translate-x-0"
-            leave="transition ease-in-out duration-300 transform"
-            leaveFrom="translate-x-0"
-            leaveTo="-translate-x-full"
-          >
+      {open && (
+        <div className="lg:hidden">
+          <div className="fixed inset-0 flex z-40">
+            <div className="fixed inset-0 bg-black bg-opacity-25" aria-hidden="true"></div>
             <div className="relative max-w-xs w-full bg-white shadow-xl pb-12 flex flex-col overflow-y-auto">
               <div className="px-4 pt-5 pb-2 flex">
                 <button
                   type="button"
-                  className="-m-2 p-2 rounded-md inline-flex items-center justify-center text-gray-400"
+                  className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400"
                   onClick={() => setOpen(false)}
                 >
                   <XIcon className="h-6 w-6" aria-hidden="true" />
                   <span className="sr-only">Close menu</span>
                 </button>
               </div>
-
-              {user ? (
-                <div className="border-t border-gray-200 py-6 px-4 space-y-6">
-                  <div className="flow-root">
+              <div className="mt-2">
+                {user ? (
+                  <div className="flex flex-col">
                     <button
                       onClick={logoutHandler}
-                      className="-m-2 p-2 block font-medium text-gray-900"
+                      className="text-sm font-medium text-gray-700 bg-neutral-100 px-2 py-3 text-center cursor-pointer hover:bg-gray-400 transition-colors duration-200 hover:text-white"
                     >
                       Sign Out
                     </button>
-                  </div>
-
-                  <div className="flow-root">
+                    <Link
+                      to="/quiz"
+                      className="text-sm font-medium text-gray-700 bg-neutral-100 px-2 py-3 text-center cursor-pointer hover:bg-gray-400 transition-colors duration-200 hover:text-white"
+                      onClick={() => setOpen(false)}
+                    >
+                      Quiz
+                    </Link>
                     <Link
                       to="/my-results"
-                      className="-m-2 p-2 block font-medium text-gray-900"
+                      className="text-sm font-medium text-gray-700 bg-neutral-100 px-2 py-3 text-center cursor-pointer hover:bg-gray-400 transition-colors duration-200 hover:text-white"
+                      onClick={() => setOpen(false)}
                     >
-                      My Results
+                      Results
                     </Link>
-                  </div>
-
-                  <div className="flow-root">
                     <Link
                       to="/"
-                      className="-m-2 p-2 block font-medium text-gray-900"
+                      className="text-sm font-medium text-gray-700 bg-neutral-100 px-2 py-3 text-center cursor-pointer hover:bg-gray-400 transition-colors duration-200 hover:text-white"
+                      onClick={() => setOpen(false)}
                     >
                       Home
                     </Link>
+                    {user.is_admin && (
+                      <Link
+                        to="/admin"
+                        className="text-sm font-medium text-gray-700 bg-neutral-100 px-2 py-3 text-center cursor-pointer hover:bg-gray-400 transition-colors duration-200 hover:text-white"
+                        onClick={() => setOpen(false)}
+                      >
+                        Admin
+                      </Link>
+                    )}
                   </div>
-
-                  <div className="flow-root">
-                    <Link
-                      to="/quiz"
-                      className="-m-2 p-2 block font-medium text-gray-900"
-                    >
-                      Quizes
-                    </Link>
-                  </div>
-
-                  <div className="flow-root">
-                    <Link
-                      to="/admin"
-                      className="-m-2 p-2 block font-medium text-gray-900"
-                    >
-                      Admin
-                    </Link>
-                  </div>
-                </div>
-              ) : (
-                <div className="border-t border-gray-200 py-6 px-4 space-y-6">
-                  <div className="flow-root">
+                ) : (
+                  <div className="flex flex-col">
                     <Link
                       to="/login"
-                      className="-m-2 p-2 block font-medium text-gray-900"
+                      className="text-sm font-medium text-gray-700 bg-neutral-100 px-2 py-3 text-center cursor-pointer hover:bg-gray-400 transition-colors duration-200 hover:text-white"
+                      onClick={() => setOpen(false)}
                     >
                       Sign in
                     </Link>
-                  </div>
-                  <div className="flow-root">
                     <Link
                       to="/register"
-                      className="-m-2 p-2 block font-medium text-gray-900"
+                      className="text-sm font-medium text-gray-700 bg-neutral-100 px-2 py-3 text-center cursor-pointer hover:bg-gray-400 transition-colors duration-200 hover:text-white"
+                      onClick={() => setOpen(false)}
                     >
                       Create account
                     </Link>
                   </div>
-                </div>
-              )}
+                )}
+              </div>
             </div>
-          </Transition.Child>
-        </Dialog>
-      </Transition.Root>
+          </div>
+        </div>
+      )}
+
 
       <header className="relative overflow-hidden">
         {/* Top navigation */}
